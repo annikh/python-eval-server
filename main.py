9bdde08e3846cecd76394d688aef92e284772aca
Anniken Holst <annikekh@stud.ntnu.no>
2019-03-21 13:26:27 +0100

    Allow cross origin requests

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import random
 from flask import Flask, request
+from flask_cors import CORS
 import sys
 from io import StringIO
 import contextlib
 
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/run')
 def run_code():
     code = request.args.get('code')
+    print("code: ", code)
     with stdoutIO() as s:
         exec(code)
+    print("value: ", s.getvalue())
     return s.getvalue()
 
 
